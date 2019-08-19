92b6ba9b0d55709d2de42087fd1ca476af380386
Munandar28 <Munandar28>
2019-08-19 14:23:35 +0700

    new project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,293 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/table.css') }}">
+</head>
+    <body>
+
+    <h2>Kumpulan CV Anak Magang</h2>
+
+    <table>
+        <tr>
+            <th>Nama</th>
+            <th>Projek</th>
+            <th>Tools</th>
+            <th>Link CV(PDF)</th>
+        </tr>
+
+        <tr>
+            <td>Kevin Munandar</td>
+            <td><p>1. Big Data Exploration</p>
+                <p>2. CellCom Malaysia</p>
+                <p>3. OSS Project</p>
+                <p>4. Redsite Project</p></td>
+                <p>5. Data Analytics (PT Pupuk Indonesia)</p>
+            <td><p>1. Google Drive</p>
+                <p>2. Navicat</p>
+                <p>3. SSH-Client(PuTTY)</p>
+                <p>4. Postgresql</p>
+                <p>5. Apache Airflow</p>
+                <p>6. MapInfo</p>
+                <p>7. FileZilla</p>
+                <p>8. Hadoop Hortonworks</p>
+                <p>9. Visual Code</p></td>
+            <td><button type="button"><a href="/assets/Kevin's Resume.pdf">example 101</button></a></td>
+        </tr>
+        
+        <tr>
+            <td>Muhamad Fadel</td>
+            <td><p>1. Big Data Exploration</p>
+                <p>2. CellCom Malaysia</p>
+                <p>3. OSS Project</p>
+                <p>4. Redsite Project</p></td>
+            <td><p>1. Google Drive</p>
+                <p>2. Hadoop Hortonworks</p>
+            <td><a href="/assets/Muhamad Fadel - cv.pdf">example 101</a></td>
+        </tr>
+
+        <tr>
+            <td>Maykel Wongkar</td>
+            <td><p>1. IT Support : Open Docman</p>
+                <p>2. Rekonsiliasi Payment Muslim Pocket</p>
+            <td><p>1. Visual Studio Code</p>
+                <p>2. Navicat</p>
+            <td><a href="/assets/Maykel's Resume(1).pdf">example 101</a></td>
+        </tr>
+
+        <tr>
+            <td>Chalvin Dinata</td>
+            <td><p>1. Rekonsiliasi Payment Muslim Pocket</p>>
+            <td><p>1. Visual Studio Code</p>
+                <p>2. Navicat</p>
+            <td><a href="/assets/Chalvin's Resume.pdf">example 101</a></td>
+        </tr>
+
+        <tr>
+            <td>Stephanie Wilhelmina</td>
+            <td><p>1. MuslimPocket</p>
+                <p>2. Jaring Laut Application</p>
+            <td><p>1. Visual Studio Code</p>
+                <p>2. Navicat</p>
+            <td><a href="/assets/Stephanie_Wilhelmina_CV.pdf">Resume</a></td>
+        </tr>
+
+        <tr>
+            <td>Inez Christiani</td>
+            <td><p>1. MuslimPocket(iOS)</p>
+                <p>2. Jaring Laut Application</p>
+                <p>2. Incubator : Muslim Pocket</p>
+            <td><p>1. Trello Scrum</p>
+                <p>2. Immobi Tracker</p>
+            <td><a href="/assets/CV - Inez Christiani.pdf">Resume</a></td>
+        </tr>
+        
+        <tr>
+            <td>Muhammad Fardiansyah</td>
+            <td><p>1. Incubator : Muslim Pocket</p>
+            <td><p>1. Trello Scrum</p>
+                <p>2. Google Drive</p>
+            <td><a href="/assets/CV_Muhammad_fardiansyah.pdf">Resume</a></td>
+        </tr>
+
+        <tr>
+            <td>Adlez Devha Bernandez Rangkinaung</td>
+            <td><p>1. Redsite Project</p>
+                <p>2. CRM App (Sales and Marketing App)</p>
+                <p>3. Incubator : Angel Investment</p>
+                <p>4. Muslim Pocket</p>
+            <td><p>1. Trello Scrum</p>
+                <p>2. Google Drive</p>
+                <p>3. Immobi Tracker</p>
+            <!-- <td><a href="/assets/CV_Muhammad_fardiansyah.pdf">Resume</a></td> -->
+        </tr>
+
+        <tr>
+            <td>Justin Tanner</td>
+            <td><p>1. Rating Karyawan(iOS)</p>
+                <p>2. CRM App (Sales and Marketing App)</p>
+                <p>3. Incubator : Angel Investment</p>
+            <td><p>1. Xcode</p>
+                <p>2. Google Drive</p>
+                <p>3. Immobi Tracker</p>
+            <td><a href="/assets/Justin's Resume.pdf">Resume</a></td>
+        </tr>
+
+        <tr>
+            <td>Martinus G B L Parera</td>
+            <td><p>1. Muslim Pocket(Android)</p>
+                <p>2. Jaring Laut Application</p>
+            <td><p>1. Xcode</p>
+                <p>2. Google Drive</p>
+                <p>3. Immobi Tracker</p>
+            <!-- <td><a href="/assets/CV_Muhammad_fardiansyah.pdf">Resume</a></td> -->
+        </tr>
+        
+        <tr>
+            <td>Mohammad Akbar</td>
+            <td><p>1. Immobi Tracker (Web)</p>
+                <p>2. Nominatif Entertaint</p>
+                <p>3. Rekonsiliasi Payment Muslim Pocket</p>
+                <p>4. Muslim Pocket</p>
+            <td><p>1. Postman</p>
+                <p>2. Visual Studio Code</p>
+                <p>3. Navicat</p>
+                <p>4. Terminal</p>
+                <p>5. GitLab / GitHub</p>
+            <td><a href="/assets/Cv_akbar2019.pdf">Resume</a></td>
+        </tr>
+        
+        <tr>
+            <td>Aditya Pradipta</td>
+            <td><p>1. Immobi Tracker (Web)</p>
+                <p>2. Nominatif Entertaint</p>
+                <p>3. Rekonsiliasi Payment Muslim Pocket</p>
+            <td><p>1. Trello Scrum</p>
+                <p>2. Immobi Tracker</p>
+                <p>3. Google Drive</p>
+            <td><a href="/assets/CV-Aditya Pradipta.pdf">Resume</a></td>
+        </tr>
+        
+        <tr>
+            <td>Bob Harwin</td>
+            <td><p>1. Immobi Tracker (Web)</p>
+                <p>2. Nominatif Entertaint</p>
+                <p>3. Rekonsiliasi Payment Muslim Pocket</p>
+            <td><p>1. Postman</p>
+                <p>2. Visual Studio Code</p>
+                <p>3. Navicat</p>
+                <p>4. Terminal</p>
+                <p>5. GitLab / GitHub</p>
+            <td><a href="/assets/Bob's Resume.pdf">Resume</a></td>
+        </tr>
+        
+        <tr>
+            <td>Bagas Kuncoro</td>
+            <td><p>3. Rekonsiliasi Payment Muslim Pocket</p>
+            <td><p>1. Postman</p>
+                <p>2. Visual Studio Code</p>
+                <p>3. Navicat</p>
+                <p>4. Terminal</p>
+                <p>5. GitLab / GitHub</p>
+            <!-- <td><a href="/assets/Cv_akbar2019.pdf">Resume</a></td> -->
+        </tr>
+        
+        <tr>
+            <td>Hestiara Agatha </td>
+            <td><p>1. CRM App(Sales and Marketing App)</p>
+                <p>2. Immobi Tracker(Web)</p>
+            <td><p>1. Android Studio</p>
+                <p>2. Visual Studio Code</p>
+                <p>3. Navicat</p>
+            <td><a href="/assets/hestiara-agatha-cv.pdf">Resume</a></td>
+        </tr>
+        
+        <tr>
+            <td>Mohammad Alhafidz Permana Baskoro</td>
+            <td><p>1. Immobi Tracker (iOS)</p>
+                <p>4. Muslim Pocket(iOS)</p>
+            <td><p>1. Postman</p>
+                <p>2. Visual Studio Code</p>
+                <p>3. Navicat</p>
+                <p>4. Terminal</p>
+                <p>5. GitLab / GitHub</p>
+                <p>6. Xcode</p>
+            <td><a href="/assets/Cv_Mohammad_Alhafidz_Permana_Baskoro.pdf">Resume</a></td>
+        </tr>
+        
+        <tr>
+            <td>Andri Yanto</td>
+            <td><p>1. CRM App(Sales and Marketing App)</p>
+                <p>2. Rating Karyawan(Android)</p></p>
+            <td><p>1. Android Studio</p>
+                <p>2. Visual Studio Code</p>
+            <td><a href="/assets/Andri's Resume (1).pdf">Resume</a></td>
+        </tr>
+        
+        <tr>
+            <td>Adrian Hartanto</td>
+            <td><p>1. Immobi Tracker(Web)</p>
+                <p>2. Rating Karyawan(Web)</p>
+            <td><p>1. Postman</p>
+                <p>2. Visual Studio Code</p>
+                <p>3. Navicat</p>
+                <p>4. Terminal</p>
+                <p>5. GitLab / GitHub</p>
+            <td><a href="/assets/CV Adrian Hartanto.jpg">Resume</a></td>
+        </tr>
+        
+        <tr>
+            <td>Ivander Reyner</td>
+            <td><p>1. Immobi Tracker(Web)</p>
+                <p>2. Rating Karyawan(Web)</p>
+            <td><p>1. Postman</p>
+                <p>2. Visual Studio Code</p>
+                <p>3. Navicat</p>
+                <p>4. Terminal</p>
+                <p>5. GitLab / GitHub</p>
+            <td><a href="/assets/Ivander's Resume.pdf">Resume</a></td>
+        </tr>
+        
+        <tr>
+            <td>Aulia Prameswari</td>
+            <td><p>1. Muslim Pocket</p>
+                <p>2. Rating Karyawan(Web)</p>
+                <p>3. Redsite Project</p>
+            <td><p>1. Trello Scrum</p>
+                <p>2. Immobi Tracker</p>
+                <p>3. Google Drive</p>
+            <td><a href="/assets/Aulia's Resume.pdf">Resume</a></td>
+        </tr>
+        
+        <tr>
+            <td>Samuel Sanjaya</td>
+            <td><p>1. Rating Karyawan</p>
+                <p>2. CRM App(Sales and Marketing App)</p>
+            <td><p>1. Android Studio</p>
+            <td><a href="/assets/Samuel Sanjaya CV.pdf">Resume</a></td>
+        </tr>
+        
+        <tr>
+            <td>Aprilia Dwi Lestari</td>
+            <td><p>1. Muslim Pocket</p>
+                <p>2. Data Analysis</p>
+            <td><p>1. Trello Scrum</p>
+                <p>2. Immobi Tracker</p>
+                <p>3. Google Drive</p>
+            <td><a href="/assets/Aprilia Dwi.L (3).pdf">Resume</a></td>
+        </tr>
+        
+        <tr>
+            <td>Imanuel Ruben</td>
+            <td><p>1. Data Analytics (PT Pupuk Indonesia)</p>
+            <td><p>1. Anaconda</p>
+                <p>2. Jupyter Notebook</p>
+            <td><a href="/assets/Imanuel Ruben.pdf">Resume</a></td>
+        </tr>
+
+        <tr>
+            <td>Hengky</td>
+            <td><p>1. Data Analytics (PT Pupuk Indonesia)</p>
+            <td><p>1. Anaconda</p>
+                <p>2. Jupyter Notebook</p>
+            <td><a href="/assets/Hengky.pdf">Resume</a></td>
+        </tr>
+
+        <tr>
+            <td>Krisna Fernando</td>
+            <td><p>1. Data Analytics (PT Pupuk Indonesia)</p>
+            <td><p>1. Anaconda</p>
+                <p>2. Jupyter Notebook</p>
+            <td><a href="/assets/Krisna Fernando.pdf">Resume</a></td>
+        </tr>
+        
+        <tr>
+            <td>Muhammad Farid Wajidi</td>
+            <td><p>1. Incubator : Muslim Pocket</p>
+            <td><p>1. Trello Scrum</p>
+                <p>2. Google Drive</p>
+            <td><a href="/assets/Aulia's Resume.pdf">Resume</a></td>
+        </tr>
+
+    </table>
+    </body>
+</html>
