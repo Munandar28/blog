a60a146f5afa71acbcd1ff68fe23b6a385bcd616
Munandar28 <Munandar28>
2019-08-20 14:19:36 +0700

    Perubahan ketujuh welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,6 +40,7 @@
                 <p>2. CellCom Malaysia</p>
                 <p>3. OSS Project</p>
                 <p>4. Redsite Project</p></td>
+                <p>5. Data Analytics (PT Pupuk Indonesia)</p>
             <td><p>1. Google Drive</p>
                 <p>2. Hadoop Hortonworks</p>
             <td><a href="/assets/Muhamad Fadel - cv.pdf">Resume</a></td>
